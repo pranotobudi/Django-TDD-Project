5ed91aac6538c39fb25664ad64c84002780aab63
pranotobudi <pranotobudi.s@gmail.com>
2019-04-17 17:43:35 +0700

    re-work chapter 02

--- functional_tests.py
+++ functional_tests.py
@@ -8,6 +8,9 @@ import unittest
 # assert 'To-Do' in browser.title
 # browser.quit()
 
+browser = webdriver.Firefox()
+browser.get('http://localhost:8000')
+assert 'To-Do' in browser.title
 
 class NewVisitorTest(unittest.TestCase):
     def setUp(self):
@@ -16,10 +19,10 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
-    def check_for_row_in_list_table(self, row_text):
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn(row_text, [row.text for row in rows])
+    # def check_for_row_in_list_table(self, row_text):
+    #     table = self.browser.find_element_by_id('id_list_table')
+    #     rows = table.find_elements_by_tag_name('tr')
+    #     self.assertIn(row_text, [row.text for row in rows])
 
     def test_can_start_a_list_and_retrieve_it_later(self):
         # Edith has heard about a cool new online to-do app. She goes
@@ -28,40 +31,41 @@ class NewVisitorTest(unittest.TestCase):
 
         # She notices the page title and header mention to-do lists
         self.assertIn('To-Do', self.browser.title)
-        header_text = self.browser.find_element_by_tag_name('h1').text  
-        self.assertIn('To-Do', header_text)
+        self.fail('Finish the test!')
+    #     header_text = self.browser.find_element_by_tag_name('h1').text  
+    #     self.assertIn('To-Do', header_text)
 
-        # She is invited to enter a to-do item straight away
-        inputbox = self.browser.find_element_by_id('id_new_item')
-        self.assertEqual(
-            inputbox.get_attribute('placeholder'),
-            'Enter a to-do item'
-        )
+    #     # She is invited to enter a to-do item straight away
+    #     inputbox = self.browser.find_element_by_id('id_new_item')
+    #     self.assertEqual(
+    #         inputbox.get_attribute('placeholder'),
+    #         'Enter a to-do item'
+    #     )
 
-        # She types "Buy peacock feathers" into a text box (Edith's hobby
-        # is tying fly-fishing lures)
-        inputbox.send_keys('Buy peacock feathers')
+    #     # She types "Buy peacock feathers" into a text box (Edith's hobby
+    #     # is tying fly-fishing lures)
+    #     inputbox.send_keys('Buy peacock feathers')
 
-        # When she hits enter, the page updates, and now the page lists
-        # "1: Buy peacock feathers" as an item in a to-do list
-        inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
-        # self.check_for_row_in_list_table('1: Buy peacock feathers')
+    #     # When she hits enter, the page updates, and now the page lists
+    #     # "1: Buy peacock feathers" as an item in a to-do list
+    #     inputbox.send_keys(Keys.ENTER)
+    #     time.sleep(1)
+    #     # self.check_for_row_in_list_table('1: Buy peacock feathers')
 
-        # There is still a text box inviting her to add another item. She
-        # enters "Use peacock feathers to make a fly" (Edith is very methodical)
-        inputbox = self.browser.find_element_by_id('id_new_item')
-        inputbox.send_keys('Use peacock feathers to make a fly')
-        inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
+    #     # There is still a text box inviting her to add another item. She
+    #     # enters "Use peacock feathers to make a fly" (Edith is very methodical)
+    #     inputbox = self.browser.find_element_by_id('id_new_item')
+    #     inputbox.send_keys('Use peacock feathers to make a fly')
+    #     inputbox.send_keys(Keys.ENTER)
+    #     time.sleep(1)
     
-        # The page updates again, and now shows both items on her list
-        self.check_for_row_in_list_table('1: Buy peacock feathers')
-        self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
+    #     # The page updates again, and now shows both items on her list
+    #     self.check_for_row_in_list_table('1: Buy peacock feathers')
+    #     self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
 
 
-        self.fail('Finish the Test!') 
-        #self.fail will be executed if no error happened
+    #     self.fail('Finish the Test!') 
+    #     #self.fail will be executed if no error happened
 
 
 # Edith wonders whether the site will remember her list. Then she sees
