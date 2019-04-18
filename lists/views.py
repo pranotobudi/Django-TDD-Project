e75f2949c8b110c70aedd73408018c40bb5de766
pranotobudi <pranotobudi.s@gmail.com>
2019-04-18 17:29:00 +0700

    basic view now returns minimal HTML

--- lists/views.py
+++ lists/views.py
@@ -2,8 +2,8 @@ from django.shortcuts import redirect, render
 from django.http import HttpResponse 
 from lists.models import Item
 
-def home_page():
-    pass
+def home_page(request):
+    return HttpResponse('<html><title>To-Do lists</title></html>')
 # def home_page(request):
 #     if request.method == 'POST':
 #         Item.objects.create(text=request.POST['item_text']) 
